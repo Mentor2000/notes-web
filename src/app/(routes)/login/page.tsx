d8de9f0ddf9a2b59521e3361f5060622fbcd0afa
Mentor2000 <dmspanaretos@gmail.com>
2025-09-07 19:29:49 +0300

    Add files via upload

---
+++ src/app/(routes)/login/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { supabase } from '../../../lib/supabase';
+
+export default function Login(){ 
+  return (<main><h1>Login Page</h1></main>);
+}
---
+++ src/app/(routes)/upload/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { supabase } from '../../../lib/supabase';
+
+export default function Upload(){ 
+  return (<main><h1>Upload Page</h1></main>);
+}
